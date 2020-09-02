6d645af3c4ca49cd99fe06b50a7ac9fc34c74bfc
Keisuke Miyako <keisuke.miyako@4d.com>
2020-09-03 08:17:17 +0900

    2.1.0

--- xlsxio/4DPlugin-XLSXIO.cpp
+++ xlsxio/4DPlugin-XLSXIO.cpp
@@ -10,6 +10,95 @@
 
 #include "4DPlugin-XLSXIO.h"
 
+/*
+ https://qiita.com/kumagi/items/f45742d9a312e27769b1
+ */
+
+//   Char. number range  |        UTF-8 octet sequence
+//      (hexadecimal)    |              (binary)
+//   --------------------+---------------------------------------------
+//   0000 0000-0000 007F | 0xxxxxxx
+//   0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+//   0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+//   0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+bool decode_unicode_escape_to_utf8(const std::string& src, std::string& dst) {
+  std::string result("");
+  result.resize(src.size());
+
+  for (size_t i = 0; i < result.size(); ++i) {
+    result[i] = '\0';
+  }
+
+  size_t result_index = 0;
+  bool in_surrogate_pair = false;
+  size_t surrogate_buffer = 0;
+  for (size_t i = 0; i < src.size(); ++i) {
+    if (i + 1 < src.size() && src[i] == '\\' && src[i+1] == 'u') {
+      size_t octet = 0;
+      {  // calculate octet
+        const char* const hex = &src[i + 2];
+        for (int j = 0; j < 4; ++j) {
+          octet *= 16;
+          if ('0' <= hex[j] && hex[j] <= '9') {
+            octet += static_cast<int>(hex[j] - '0');
+          } else if ('a' <= hex[j] && hex[j] <= 'f') {
+            octet += static_cast<int>(hex[j] - 'a' + 10);
+          } else if ('A' <= hex[j] && hex[j] <= 'F') {
+            octet += static_cast<int>(hex[j] - 'A' + 10);
+          } else {
+            return false;
+          }
+        }
+      }
+      {  // fill up sequence
+        char* const sequence = &result[result_index];
+        if (in_surrogate_pair) {
+          if (0xdc00 <= octet && octet <= 0xdfff) {
+            // low surrogate pair
+            const size_t joined = surrogate_buffer + (octet & 0x03ff) + 0x10000;
+            sequence[0] = (static_cast<char>(joined >> 18)  &  0x3) | 0xf0;
+            sequence[1] = (static_cast<char>(joined >> 12)  & 0x3f) | 0x80;
+            sequence[2] = (static_cast<char>(joined >> 6)   & 0x3f) | 0x80;
+            sequence[3] = (static_cast<char>(joined & 0xff) & 0x3f) | 0x80;
+            result_index += 4;
+            in_surrogate_pair = false;
+          } else {
+            return false;
+          }
+        } else if (octet < 0x7f) {
+          sequence[0] = static_cast<char>(octet) & 0x7f;
+          result_index += 1;
+        } else if (octet < 0x7ff) {
+          sequence[0] = (static_cast<char>(octet >> 6) & 0xdf) | 0xc0;
+          sequence[1] = (static_cast<char>(octet)      & 0x3f) | 0x80;
+          result_index += 2;
+        } else if (0xdbff) {
+          // high surrogate pair
+          in_surrogate_pair = true;
+          surrogate_buffer = (octet & 0x03ff) * 0x400;
+        } else {
+          sequence[0] = (static_cast<char>(octet >> 12) & 0x0f) | 0xe0;
+          sequence[1] = (static_cast<char>(octet >> 6)  & 0x3f) | 0x80;
+          sequence[2] = (static_cast<char>(octet)       & 0x3f) | 0x80;
+          result_index += 3;
+        }
+      }
+      i += 5;  // \\uXXXX is 6 bytes, so + 5 here, and + 1 in next loop
+    } else {  // not unicode
+      if (in_surrogate_pair) {
+        return false;
+      }
+      result[result_index] = src[i];
+      result_index += 1;
+    }
+    // next char
+  }
+  result.resize(result_index);
+  dst.swap(result);
+  return true;
+}
+
 #pragma mark -
 
 void PluginMain(PA_long32 selector, PA_PluginParameters params) {
@@ -88,25 +177,27 @@ void XLSX_TO_JSON(PA_PluginParameters params) {
             
         }else
         {
+			Json::Value json_sheet(Json::objectValue);
+			Json::Value json_sheet_rows(Json::arrayValue);
+
+			Json::Value json_sheet_row(Json::objectValue);
+			Json::Value json_sheet_row_values(Json::arrayValue);
+
             time_t startTime = time(0);
             
             const char *sheetname;
             while ((sheetname = xlsxioread_sheetlist_next(sheetlist)) != NULL)
             {
-                Json::Value json_sheet(Json::objectValue);
-                Json::Value json_sheet_rows(Json::arrayValue);
+				json_sheet.clear();
+				json_sheet_rows.clear();
                 
-				xlsxioreadersheet sheet = NULL;
-
 				if (issheetnamespecified)
 				{
-					sheet = xlsxioread_sheet_open(xlsxioread, sheetnamespecified, flags);
-				}
-				else
-				{					
-					sheet = xlsxioread_sheet_open(xlsxioread, sheetname, flags);
+					sheetname = sheetnamespecified;
 				}
                 
+				xlsxioreadersheet sheet = xlsxioread_sheet_open(xlsxioread, sheetname, flags);
+
                 if(sheet)
                 {
 					json_sheet["name"] = sheetname;
@@ -121,13 +212,13 @@ void XLSX_TO_JSON(PA_PluginParameters params) {
 							PA_YieldAbsolute();
 						}
 
-                        Json::Value json_sheet_row(Json::objectValue);
-                        Json::Value json_sheet_row_values(Json::arrayValue);
-                        
+						json_sheet_row.clear();
+						json_sheet_row_values.clear();
+
                         char *value;
                         while ((value = xlsxioread_sheet_next_cell(sheet)) != NULL)
                         {
-                            json_sheet_row_values.append(value);
+							json_sheet_row_values.append(value);
                         }
                         
                         json_sheet_row["values"] = json_sheet_row_values;
--- xlsxio/test/Project/Sources/Methods/TEST.4dm
+++ xlsxio/test/Project/Sources/Methods/TEST.4dm
@@ -1,16 +1,13 @@
 //%attributes = {}
 $path:=System folder:C487(Desktop:K41:16)+"SB_data.xlsx"
 
-  //ARRAY TEXT($names;0)
+ARRAY TEXT:C222($names;0)
 XLSX SHEET NAMES ($path;$names)
 
 For ($i;1;Size of array:C274($names))
-	
 	$json_e:=$names{$i}
 	$json:=XLSX TO JSON ($path;XLSXIOREAD_SKIP_EMPTY_ROWS;$json_e)
-	
 End for 
-
 
   //all values are returned as string
 $json:=XLSX TO JSON ($path;XLSXIOREAD_SKIP_EMPTY_ROWS;$json_e)
@@ -18,4 +15,3 @@ $json:=XLSX TO JSON ($path;XLSXIOREAD_SKIP_EMPTY_ROWS;$json_e)
 $path:=System folder:C487(Desktop:K41:16)+"SB_data_cp.xlsx"
 
 JSON TO XLSX ($path;$json)
-
